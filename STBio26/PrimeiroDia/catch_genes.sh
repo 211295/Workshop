e713e25bb3f838b00e5d401319b4f56afd21b08c
Felipe Simionato Salles <123026352+211295@users.noreply.github.com>
2026-09-24 17:18:44 -0300

    Add script to calculate sequence lengths from fasta files
    Script calculates the length of sequences in each fasta file and outputs the results to a new file.

---
+++ STBio26/PrimeiroDia/catch_genes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+###length of sequencies in a fasta file
+
+for file in *.fasta;
+do awk '/^>/ {if (seqlen){print seqlen}; print ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' $file > length_of_$file;
+done
